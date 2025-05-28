6f04cbd89ee348968e8b2a67cd14a5715eb87aca
JayRGarg <jayrgarg@gmail.com>
2025-05-28 01:17:30 -0700

    add readiness checker, refactor fileserver funcationality

--- main.go
+++ main.go
@@ -9,11 +9,18 @@ func main() {
 	filepathRoot := "."
 	port := "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handleHealthz)
 	server := http.Server{
 		Addr: 		":"+port,
 		Handler: 	mux,
 	}
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handleHealthz(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
