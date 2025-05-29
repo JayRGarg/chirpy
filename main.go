22bb9de40203980f0aca075c5347d0e7b2750d35
JayRGarg <jayrgarg@gmail.com>
2025-05-29 01:12:19 -0700

    add functional middleware handlers

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"sync/atomic"
 	"net/http"
 )
 
@@ -9,7 +11,12 @@ func main() {
 	filepathRoot := "."
 	port := "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	cfg := &apiConfig{}
+
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.Handle("/metrics", cfg.middlewareMetricsGet())
+	mux.Handle("/reset", cfg.middlewareMetricsReset())
 	mux.HandleFunc("/healthz", handleHealthz)
 	server := http.Server{
 		Addr: 		":"+port,
@@ -17,6 +24,43 @@ func main() {
 	}
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+type IncHitsHandler struct{
+	inner 		http.Handler
+	cfg 		*apiConfig
+}
+func (oH *IncHitsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	oH.cfg.fileserverHits.Add(1)
+	oH.inner.ServeHTTP(w, r)
+}
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return &IncHitsHandler{inner: next, cfg: cfg}
+}
+
+type GetHitsHandler struct{
+	cfg 		*apiConfig
+}
+func (oH *GetHitsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("Hits: %v", oH.cfg.fileserverHits.Load())))
+}
+func (cfg *apiConfig) middlewareMetricsGet() http.Handler {
+	return &GetHitsHandler{cfg: cfg}
+}
+
+type ResetHitsHandler struct{
+	cfg 		*apiConfig
+}
+func (oH *ResetHitsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	oH.cfg.fileserverHits.Store(0)
+}
+
+func (cfg *apiConfig) middlewareMetricsReset() http.Handler {
+	return &ResetHitsHandler{cfg: cfg}
 }
 
 func handleHealthz(w http.ResponseWriter, req *http.Request) {
