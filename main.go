97a1c83a04d64be32d608e52ad53902895422638
JayRGarg <jayrgarg@gmail.com>
2025-05-29 01:21:42 -0700

    refactor middleware handlers to be created from http.HandlerFunc

--- main.go
+++ main.go
@@ -30,37 +30,29 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
-type IncHitsHandler struct{
-	inner 		http.Handler
-	cfg 		*apiConfig
-}
-func (oH *IncHitsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	oH.cfg.fileserverHits.Add(1)
-	oH.inner.ServeHTTP(w, r)
-}
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return &IncHitsHandler{inner: next, cfg: cfg}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
 
-type GetHitsHandler struct{
-	cfg 		*apiConfig
-}
-func (oH *GetHitsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprintf("Hits: %v", oH.cfg.fileserverHits.Load())))
-}
 func (cfg *apiConfig) middlewareMetricsGet() http.Handler {
-	return &GetHitsHandler{cfg: cfg}
-}
-
-type ResetHitsHandler struct{
-	cfg 		*apiConfig
-}
-func (oH *ResetHitsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	oH.cfg.fileserverHits.Store(0)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+	})
 }
 
 func (cfg *apiConfig) middlewareMetricsReset() http.Handler {
-	return &ResetHitsHandler{cfg: cfg}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(0)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hits reset to 0")))
+		
+	})
 }
 
 func handleHealthz(w http.ResponseWriter, req *http.Request) {
